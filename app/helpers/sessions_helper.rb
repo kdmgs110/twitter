016c0e646454f0c93e1fca59da0f2a2f4a0139fd
Dai <kdmgs110@gmail.com>
2016-09-21 11:17:39 +0000

    chapter 9 finished

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,10 @@ module SessionsHelper
   def current_user
      @current_user ||= User.find_by(id: session[:user_id])
   end
+  def current_user?(user)
+    user == current_user
+  end
+  
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
@@ -34,4 +38,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+  
+  
 end
